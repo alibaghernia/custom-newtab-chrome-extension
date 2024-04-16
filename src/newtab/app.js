c701d69ed8d34f3363617ffabb563e51836200cd
alibaghernia <alibaghernia0@gmail.com>
2024-04-16 17:01:25 +0330

    Add link list

--- src/newtab/app.js
+++ src/newtab/app.js
@@ -3,13 +3,16 @@ let el = document.getElementById('curent-date')
 let elMyNote = document.getElementById('my-note')
 let elShowIcon = document.getElementById('show-icon')
 let elHideIcon = document.getElementById('hide-icon')
-let elSwitchBtn = document.getElementById('switch-btn') 
+let elSwitchBtn = document.getElementById('switch-btn')
+let elLinkList = document.getElementById('link-list')
+let elAddLinkBtn = document.getElementById('add-link-btn')
+
 // --------------
 let curentDate = new Date().toLocaleDateString('fa-IR')
 let dayName = new Date().toLocaleDateString('fa-IR', { weekday: 'long' })
 el.innerText = `${dayName} ${curentDate}\n${new Date()}`
 // --------------
-console.log(localStorage.getItem("my-note"))
+
 elMyNote.value = localStorage.getItem("my-note")
 
 elMyNote.addEventListener('input', function (e) {
@@ -45,3 +48,33 @@ elSwitchBtn.addEventListener('click', function (e) {
         localStorage.setItem("note-state", 'hidden')
     }
 })
+// ----------------
+elAddLinkBtn.addEventListener('click', function (e) {
+    const linkTitle = prompt('Enter link Title:')
+    const link = prompt('Enter the link:')
+    const strStoredLinkList = localStorage.getItem('link-list')
+    let storedLinkList = []
+    if (strStoredLinkList !== null) {
+        storedLinkList = JSON.parse(strStoredLinkList)
+    }
+    storedLinkList.push({
+        linkTitle,
+        link,
+    })
+    const newStrStoredLinkList = JSON.stringify(storedLinkList)
+    localStorage.setItem('link-list', newStrStoredLinkList)
+    // TODO
+    location.reload();
+})
+const linkslist = JSON.parse(localStorage.getItem('link-list')) ?? []
+let linklistItems = ''
+
+for (let index = 0; index < linkslist.length; index++) {
+    linklistItems += `
+    <div class="">
+        <a href="${linkslist[index].link}" class="block hover:underline border rounded-lg px-1">
+        ${linkslist[index].linkTitle}
+        </a>
+    </div>`
+}
+elLinkList.innerHTML = linklistItems
