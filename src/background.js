72e5ac907d913d9a03c88671c2eb40ba2e130ec1
alibaghernia <alibaghernia0@gmail.com>
2024-04-30 21:39:10 +0330

    Add  open longman & open youtube & open microsoft todo list hotkeys

--- src/background.js
+++ src/background.js
@@ -1,6 +1,20 @@
 chrome.commands.onCommand.addListener((command) => {
     if (command === 'open-translate') {
         chrome.tabs.create({ url: "https://translate.google.com/?sl=en&tl=fa&op=translate" });
+        return;
     }
+    if (command === 'open-longman') {
+        chrome.tabs.create({ url: "https://www.ldoceonline.com/" });
+        return;
+    }
+    if (command === 'open-youtube') {
+        chrome.tabs.create({ url: "https://www.youtube.com/" });
+        return;
+    }
+    if (command === 'open-todo-list') {
+        chrome.tabs.create({ url: "https://to-do.live.com/tasks/today" });
+        return;
+    }
+
 
 })
